02209500251d7fffef496e3ffcc862888b8ffc43
Khush Agrawal <149191425+Khush-Agrawal-007@users.noreply.github.com>
2026-05-12 18:14:29 +0530

    Time: 31 ms (88.24%), Space: 27.9 MB (45.76%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        nums = sorted(nums)
+        return nums
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
