a9477b6002c9240c16b338362f0a5b8c1d5158e0
Khush Agrawal <149191425+Khush-Agrawal-007@users.noreply.github.com>
2026-06-09 16:37:08 +0530

    Time: 320 ms (82.82%), Space: 0B (100%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0002-classes-with-at-least-5-students/0002-classes-with-at-least-5-students.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT class
+FROM courses
+GROUP BY class
+having count(class)>=5
+
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
